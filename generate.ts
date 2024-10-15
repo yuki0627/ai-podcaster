0e8450185027d1b8b0351776ff8ead33c2ca5562
snakajima <satoshi.nakajima@gmail.com>
2024-10-15 16:18:12 -0700

    test.mp3

--- generate.ts
+++
@@ -1,6 +0,0 @@
-import OpenAI from "openai";
-import dotenv from "dotenv";
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import path from "path";
 import OpenAI from "openai";
 import dotenv from "dotenv";
 
@@ -6,3 +8,22 @@ dotenv.config();
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
+
+const sound = async (fileName: string, input: string) => {
+  const response = await openai.audio.speech.create({
+    model: "tts-1",
+    voice: "shimmer",
+    // response_format: "aac",
+    input,
+  });
+  const buffer = Buffer.from(await response.arrayBuffer());
+  console.log(`sound generated: ${input}, ${buffer.length}`);
+  const filePath = path.resolve("./scratchpad" + fileName);
+  await fs.promises.writeFile(filePath, buffer);
+}
+
+const main = async () => {
+  await sound("test.mp3", "Hello World");
+}
+
+main();
