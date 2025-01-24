9ea6d4cd090be6a741db12e031cd087624c592d6
snakajima <satoshi.nakajima@gmail.com>
2025-01-24 07:43:45 -1000

    dotenv

--- src/images.ts
+++ src/images.ts
@@ -1,13 +1,9 @@
-import "dotenv/config";
-import fsPromise from "fs/promises";
+import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
 import {
   GraphAI,
-  AgentFilterFunction,
   GraphData,
-  ComputedNodeData,
-  StaticNodeData,
 } from "graphai";
 import * as agents from "@graphai/agents";
 // import { ttsNijivoiceAgent } from "@graphai/tts_nijivoice_agent";
@@ -16,6 +12,8 @@ import ttsNijivoiceAgent from "./agents/tts_nijivoice_agent";
 // import ttsOpenaiAgent from "./agents/tts_openai_agent";
 import { pathUtilsAgent } from "@graphai/vanilla_node_agents";
 import ffmpeg from "fluent-ffmpeg";
+
+dotenv.config();
 
 type ScriptData = {
   speaker: string;
