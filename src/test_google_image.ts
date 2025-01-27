f898a9fa7396451ae76a3a92f60c5d566f6e7df9
snakajima <satoshi.nakajima@gmail.com>
2025-01-26 15:58:31 -1000

    clean up

--- src/test_google_image.ts
+++ src/test_google_image.ts
@@ -7,31 +7,12 @@ import { GoogleAuth } from 'google-auth-library';
 // https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/imagen-api
 const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID; // Your Google Cloud Project ID
 const GOOGLE_IMAGEN_MODEL='imagen-3.0-generate-001';
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY; // Your Vertex AI API Key
-const GOOGLE_ACCESS_TOKEN = process.env.GOOGLE_ACCESS_TOKEN; // Your Vertex AI API Key
-const ENDPOINT = `https://us-central1-aiplatform.googleapis.com/v1/projects/${GOOGLE_PROJECT_ID}/locations/us-central1/publishers/google/models/${GOOGLE_IMAGEN_MODEL}:predict`;
-const ENDPOINT_WITH_KEY = `https://us-central1-aiplatform.googleapis.com/v1/projects/${GOOGLE_PROJECT_ID}/locations/us-central1/publishers/google/models/${GOOGLE_IMAGEN_MODEL}:predict?key=${GOOGLE_API_KEY}`;
-
-/*
-const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY!);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-*/
+const GOOGLE_IMAGEN_ENDPOINT = `https://us-central1-aiplatform.googleapis.com/v1/projects/${GOOGLE_PROJECT_ID}/locations/us-central1/publishers/google/models/${GOOGLE_IMAGEN_MODEL}:predict`;
 
 console.log("Project ID", GOOGLE_PROJECT_ID);
-console.log("API Key", GOOGLE_API_KEY);
-console.log("GOOGLE_ACCESS_TOKEN", GOOGLE_ACCESS_TOKEN);
 // Function to generate an image from a text prompt
-async function generateImage(prompt: string): Promise<Buffer> {
-  try {
-    const auth = new GoogleAuth({
-      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    });
-    const client = await auth.getClient();
-  
-    // Get the access token
-    const accessToken = await client.getAccessToken();
-    console.log(accessToken.token);  
-  
+async function generateImage(token: string, prompt: string): Promise<Buffer> {
+  try {  
     // Prepare the payload for the API request
     const payload = {
       instances: [{
@@ -43,10 +24,10 @@ async function generateImage(prompt: string): Promise<Buffer> {
     };
 
     // Make the API call using fetch
-    const response = await fetch(ENDPOINT, {
+    const response = await fetch(GOOGLE_IMAGEN_ENDPOINT, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${accessToken.token}`,
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(payload),
@@ -78,17 +59,18 @@ async function generateImage(prompt: string): Promise<Buffer> {
 
 // Example usage
 const main = async () => {
-  /*
-  const prompt0 = "Who is Satoshi Nakamoto?";
-  const result = await model.generateContent([prompt0]);
-  console.log(result.response.text());
-  */
+  const auth = new GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  });
+  const client = await auth.getClient();
+  const accessToken = await client.getAccessToken();
 
   const prompt = 'huge solar farm in a desert, with length of 400km and width of 5km';
 
   try {
-    const imageBuffer = await generateImage(prompt);
+    const imageBuffer = await generateImage(accessToken.token!, prompt);
     fs.writeFileSync('./output/generated_image.png', imageBuffer);
+    console.log("generated");
   } catch (error) {
     console.error('Failed to generate image:', error);
   }
