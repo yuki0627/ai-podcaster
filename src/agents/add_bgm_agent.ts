b50eb566fa22d8abc62a015f74b0b92b52521c76
isamu <isamu@to-kyo.to>
2025-02-01 19:14:50 +0900

    update parameters

--- src/agents/add_bgm_agent.ts
+++ src/agents/add_bgm_agent.ts
@@ -2,12 +2,16 @@ import { AgentFunction, AgentFunctionInfo } from "graphai";
 import ffmpeg from "fluent-ffmpeg";
 import path from "path";
 
-const addBGMAgent: AgentFunction<null, string, {voiceFile: string, filename: string, script: any}> = async ({namedInputs}) => {
-  const { voiceFile, filename, script } = namedInputs;
-  const outputFile = path.resolve("./output/" + filename + "_bgm.mp3");
-  const musicFile = path.resolve(
-    process.env.PATH_BGM ?? "./music/StarsBeyondEx.mp3",
-  );
+const addBGMAgent: AgentFunction<
+  { musicFileName: string },
+  string,
+  { voiceFile: string; outFileName: string; script: any }
+> = async ({ namedInputs, params }) => {
+  const { voiceFile, outFileName, script } = namedInputs;
+  const { musicFileName } = params;
+  const outputFile = path.resolve(outFileName);
+  const musicFile = path.resolve(musicFileName);
+
   const promise = new Promise((resolve, reject) => {
     ffmpeg.ffprobe(voiceFile, (err, metadata) => {
       if (err) {
@@ -17,9 +21,10 @@ const addBGMAgent: AgentFunction<null, string, {voiceFile: string, filename: str
 
       const speechDuration = metadata.format.duration;
       const padding = script.padding ?? 4000; // msec
-      const totalDuration = padding * 2 / 1000 + Math.round(speechDuration ?? 0);
+      const totalDuration =
+        (padding * 2) / 1000 + Math.round(speechDuration ?? 0);
       console.log("totalDucation:", speechDuration, totalDuration);
-      
+
       const command = ffmpeg();
       command
         .input(musicFile)
@@ -34,7 +39,7 @@ const addBGMAgent: AgentFunction<null, string, {voiceFile: string, filename: str
           // Trim the output to the length of speech + 8 seconds
           `[amixed]atrim=start=0:end=${totalDuration}[trimmed]`,
           // Add fade out effect for the last 4 seconds
-          `[trimmed]afade=t=out:st=${totalDuration - padding/1000}:d=${padding}`,
+          `[trimmed]afade=t=out:st=${totalDuration - padding / 1000}:d=${padding}`,
         ])
         .on("error", (err) => {
           console.error("Error: " + err.message);
