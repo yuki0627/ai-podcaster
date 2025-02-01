17133724c1e3712ba35db0e1ceb3af78095886d8
isamu <isamu@to-kyo.to>
2025-02-01 15:49:18 +0900

    combine_files_agent

---
+++ src/agents/combine_files_agent.ts
@@ -0,0 +1,60 @@
+import { AgentFunction, AgentFunctionInfo } from "graphai";
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import fs from "fs";
+
+const combineFilesAgent: AgentFunction<null, string, {script: string} > = async ({namedInputs}) => {
+  const { script } = namedInputs;
+  const outputFile = path.resolve("./output/" + script.filename + ".mp3");
+  const silentPath = path.resolve("./music/silent300.mp3");
+  const silentLastPath = path.resolve("./music/silent800.mp3");
+  const command = ffmpeg();
+  script.script.forEach((element: ScriptData, index: number) => {
+    const filePath = path.resolve("./scratchpad/" + element.filename + ".mp3");
+    const isLast = index === script.script.length - 2;
+    command.input(filePath);
+    command.input(isLast ? silentLastPath : silentPath);
+    // Measure and log the timestamp of each section
+    ffmpeg.ffprobe(filePath, (err, metadata) => {
+      if (err) {
+        console.error("Error while getting metadata:", err);
+      } else {
+        element["duration"] = metadata.format.duration! + (isLast ? 0.8 : 0.3);
+      }
+    });
+  });
+
+  const promise = new Promise((resolve, reject) => {
+    command
+      .on("end", () => {
+        console.log("MP3 files have been successfully combined.");
+        resolve(0);
+      })
+      .on("error", (err: any) => {
+        console.error("Error while combining MP3 files:", err);
+        reject(err);
+      })
+      .mergeToFile(outputFile, path.dirname(outputFile));
+  });
+
+  await promise;
+
+  const outputScript = path.resolve("./output/" + script.filename + ".json");
+  fs.writeFileSync(outputScript, JSON.stringify(script, null, 2));
+
+  return outputFile;
+};
+
+const combineFilesAgentInfo: AgentFunctionInfo = {
+  name: "combineFilesAgent",
+  agent: combineFilesAgent,
+  mock: combineFilesAgent,
+  samples: [],
+  description: "combineFilesAgent",
+  category: ["ffmpeg"],
+  author: "satoshi nakajima",
+  repository: "https://github.com/snakajima/ai-podcaster",
+  license: "MIT",
+};
+
+export default combineFilesAgentInfo;
